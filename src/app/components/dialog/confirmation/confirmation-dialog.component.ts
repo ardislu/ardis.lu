c77e6eebcdc6f8e223b1251425c72666aa87164c
Ardis Lu <ardisglu@gmail.com>
2022-06-29 20:30:41 -0700

    Fix indentation

--- src/app/components/dialog/confirmation/confirmation-dialog.component.ts
+++ src/app/components/dialog/confirmation/confirmation-dialog.component.ts
@@ -17,9 +17,9 @@ export interface ConfirmationDialogData {
 export class ConfirmationDialogComponent implements OnInit {
 
   constructor(private dialogRef: MatDialogRef<ConfirmationDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: ConfirmationDialogData) {
-      // Explicitly convert return value to boolean (to catch dialog exits resulting from page esc or clicking backdrop)
-      this.dialogRef.beforeClosed().subscribe((confirmed: boolean | undefined) => this.dialogRef.close(Boolean(confirmed)));
-    }
+    // Explicitly convert return value to boolean (to catch dialog exits resulting from page esc or clicking backdrop)
+    this.dialogRef.beforeClosed().subscribe((confirmed: boolean | undefined) => this.dialogRef.close(Boolean(confirmed)));
+  }
 
   ngOnInit(): void {
   }
