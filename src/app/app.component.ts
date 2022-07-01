d08d8023144eeaf7a14b42228baac739fe0f5e76
Ardis Lu <ardisglu@gmail.com>
2022-07-01 00:08:05 -0700

    Convert root component into standalone component
    Providers must be moved into bootstrapApplication within main.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+import { HeaderComponent } from './components/header/header.component';
 import { ThemeService } from './services/theme.service';
 
 @Component({
+  standalone: true,
+  imports: [
+    RouterModule,
+    HeaderComponent
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  constructor(private theme: ThemeService) { }
-
+  constructor(private theme: ThemeService, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('logo', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/logo.svg'));
+    iconRegistry.addSvgIcon('login', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/login.svg'));
+    iconRegistry.addSvgIcon('settings', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/settings.svg'));
+    iconRegistry.addSvgIcon('info', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/info.svg'));
+    iconRegistry.addSvgIcon('github', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/github.svg'));
+    iconRegistry.addSvgIcon('keyboard_arrow_right', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/keyboard_arrow_right.svg'));
+    iconRegistry.addSvgIcon('dice', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/dice.svg'));
+    iconRegistry.addSvgIcon('launch', sanitizer.bypassSecurityTrustResourceUrl('../assets/images/launch.svg'));
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { bootstrapApplication, BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
+import { AuthGuard } from './app/guards/auth.guard';
 import { environment } from './environments/environment';
 
 import 'hammerjs';
@@ -10,5 +14,29 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+/* eslint-disable max-len */
+const routes: Routes = [
+  { path: '', loadComponent: () => import('./app/pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: 'settings', loadComponent: () => import('./app/pages/settings/settings.component').then(m => m.SettingsComponent), canActivate: [AuthGuard] },
+  { path: 'about', loadComponent: () => import('./app/pages/about/about.component').then(m => m.AboutComponent) },
+  { path: 'image-generator', loadComponent: () => import('./app/pages/image-generator/image-generator.component').then(m => m.ImageGeneratorComponent) },
+  { path: 'bfi', loadComponent: () => import('./app/pages/bfi/bfi.component').then(m => m.BfiComponent) },
+  { path: 'snek', loadComponent: () => import('./app/pages/snek/snek.component').then(m => m.SnekComponent) },
+  { path: 'colab-hosting', loadComponent: () => import('./app/pages/colab-hosting/colab-hosting.component').then(m => m.ColabHostingComponent) },
+  { path: '**', pathMatch: 'full', loadComponent: () => import('./app/pages/not-found/not-found.component').then(m => m.NotFoundComponent) }
+];
+/* eslint-enable max-len */
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', preloadingStrategy: PreloadAllModules }),
+      BrowserModule,
+      HammerModule,
+      BrowserAnimationsModule,
+      ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
+      HttpClientModule
+    )
+  ]
+});
